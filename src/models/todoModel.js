cb5695d0ac52e58afc166d95edd01eedd0f20a53
a951racer <jon.hobbs@minnykid.com>
2019-01-30 15:00:58 -0600

    Tweak

--- src/models/todoModel.js
+++ src/models/todoModel.js
@@ -3,11 +3,11 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 export const TodoSchema = new Schema({
-    order: {
-        type: Number,
+    AddedItem: {
+        type: String,
         required: 'Enter the order of the meal within the day'
     },
-    name: {
+    CreateTime: {
         type: String,
         required: 'Enter a meal name'
     }
